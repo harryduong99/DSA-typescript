47e1155cf031967015526884c1f79b98fb4c19c5
harryduong99 <duongminhnam99@gmail.com>
2024-07-30 12:10:17 +0700

    fix: add LPS

---
+++ dynamicProgramming/longestPalindromeSubsequence.ts
@@ -0,0 +1,10 @@
+// LPS
+// LPS(a) = LCS(a, reserve(a))
+function longestPalindromeSubsequence(s: string) {
+  // => get longest common subsequence of string s and its reversed version => that is the longest palindromic subsequence of s
+}
+
+// bbba => bbbb
+// bbcbab => bbcbb
+
+console.log(longestPalindromeSubsequence("bbba"));
