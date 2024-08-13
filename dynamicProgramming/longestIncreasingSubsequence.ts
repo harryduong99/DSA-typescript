c848a48f3d03ac3514efe1e182babab6182f98db
harryduong99 <duongminhnam99@gmail.com>
2024-08-13 13:29:36 +0700

    fix: LIS with recursion and memorization

--- dynamicProgramming/longestIncreasingSubsequence.ts
+++ dynamicProgramming/longestIncreasingSubsequence.ts
@@ -40,4 +40,64 @@ export const longestIncreasingSubsequence = (nums: number[]) => {
 };
 
 // console.log(longestIncreasingSubsequence([5, 2, 8, 6, 3, 6, 9, 5]));
-console.log(longestIncreasingSubsequence([6, 7, 8, 2]));
+// console.log(longestIncreasingSubsequence([6, 7, 8, 2]));
+
+// Recursion
+const LISRecursion = (nums: number[]) => {
+  let max = 1;
+  for (let i = 0; i < nums.length; i++) {
+    max = Math.max(max, recursion(nums, i));
+  }
+  return max;
+};
+
+const recursion = (nums: number[], i: number): number => {
+  if (i == 0) {
+    return 1;
+  }
+
+  let max = 1;
+  for (let prev = 0; prev < i; prev++) {
+    if (nums[prev] < nums[i]) {
+      max = Math.max(max, recursion(nums, prev) + 1);
+    }
+  }
+
+  return max;
+};
+
+console.log(LISRecursion([6, 7, 8, 2]));
+
+// Recursion + Memorization
+const LISRecursionMemo = (nums: number[]) => {
+  let max = 1;
+  let dp: number[] = Array(nums.length).fill(-1);
+  // dp[0] = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    max = Math.max(max, recursionMemo(nums, i, dp));
+  }
+  console.log(dp);
+  return max;
+};
+
+const recursionMemo = (nums: number[], i: number, dp: number[]): number => {
+  if (i == 0) {
+    return 1;
+  }
+
+  if (dp[i] != -1) {
+    return dp[i];
+  }
+
+  let max = 1;
+  for (let prev = 0; prev < i; prev++) {
+    if (nums[prev] < nums[i]) {
+      max = Math.max(max, recursionMemo(nums, prev, dp) + 1);
+    }
+  }
+  dp[i] = max; // store result of this i
+
+  return dp[i];
+};
+console.log(LISRecursionMemo([6, 7, 8, 2]));
